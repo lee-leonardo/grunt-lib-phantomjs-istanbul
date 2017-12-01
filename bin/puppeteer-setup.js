5560b4c3beb7f2cfdd56ac1fc3464393861c23da
leolee <leolee@expedia.com>
2017-11-30 16:48:57 -0800

    deleting unused file, changes to the puppeteer-setup for defaults.

--- bin/puppeteer-setup.js
+++ bin/puppeteer-setup.js
@@ -1,13 +1,22 @@
-function launchOptions(puppeteerOptions) {
+function launchOptions(launcherOptions) {
   const defaults = {
     headless: true,   // the default
     timeout: 10000,   // the original default is 30,000 i.e. 30 seconds
     dumpio: true,     // allows for grunt to 'talk to it'
   };
 
-  return Object.assign(defaults, puppeteerOptions);
+  return Object.assign(defaults, launcherOptions);
 };
 
+function viewPortOptions(viewportOptions) {
+  const defaults = {
+
+  };
+
+  return Object.assign(defaults, viewportOptions);
+}
+
 module.exports = {
-  launchOptions: launchOptions
+  launchOptions: launchOptions,
+  viewPortOptions: viewPortOptions
 };
